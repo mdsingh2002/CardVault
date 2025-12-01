a1179708a647dffb2edaf2e005878ab0d88348d5
Mark Singh <markismtuesday@gmail.com>
2025-12-01 00:36:12 -0700

    Set up Spring Boot and the project structure for the back end

---
+++ backend/src/main/java/com/cardvault/CardVaultApplication.java
@@ -0,0 +1,12 @@
+package com.cardvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardVaultApplication.class, args);
+    }
+}
