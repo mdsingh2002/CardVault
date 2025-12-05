7296a57d41d97081f9931061eb07ce49080643f4
Mark Singh <markismtuesday@gmail.com>
2025-12-04 23:53:22 -0700

    Achievement repo

---
+++ backend/src/main/java/com/cardvault/repository/AchievementRepository.java
@@ -0,0 +1,15 @@
+package com.cardvault.repository;
+
+import com.cardvault.model.Achievement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+
+    Optional<Achievement> findByName(String name);
+
+    boolean existsByName(String name);
+}
