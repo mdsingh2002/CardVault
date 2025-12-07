18c1782643a61a68ff86dfebe9dbc452c2027642
Mark Singh <markismtuesday@gmail.com>
2025-12-06 19:32:03 -0700

    Install Sonner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,6 +10,7 @@ import Collection from './pages/Collection';
 function App() {
   return (
     <Router>
+      <Toaster position="bottom-right" richColors />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
